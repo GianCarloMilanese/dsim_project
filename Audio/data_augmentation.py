acd1f5ad753f7eb7a898ae0cefd85bf1eee1632c
hechmik <hechmi.khaled1995@gmail.com>
2020-06-15 20:37:14 +0200

    Test data augm function with both recordings dir and fix params

--- Audio/data_augmentation.py
+++ Audio/data_augmentation.py
@@ -1,10 +1,7 @@
 import numpy as np
 import librosa
 import os
-import json
 
-MIN_STDEV = 0.001
-MAX_STDEV = 0.05
 MIN_PITCH_STEP = -6
 MAX_PITCH_STEP = 5
 RATE = 8000
@@ -63,7 +60,7 @@ def augment_audio_with_pitch_shift(audio_signal, sampling_rate, min_pitch_shift,
     return augmented_pitch_shift_audio_tracks
 
 
-def enrich_dataset(audio_dir, mode, n_noise, n_pitch, max_length=999999, store_tracks=False):
+def enrich_dataset(audio_dir: str, mode: str, n_noise: int, n_pitch: int, recordings_made_by_us: bool, max_length=999999):
     """
     Augment all recordings in the target directory
     :param audio_dir: path where audio tracks are stored
@@ -71,10 +68,16 @@ def enrich_dataset(audio_dir, mode, n_noise, n_pitch, max_length=999999, store_t
     :param n_noise: number of "random noise" tracks to produce from one recording
     :param n_pitch: number of "modified pitch" tracks to produce from one recording
     :param max_length: maximum length of a recording should have in order to be considered
-    :param store_tracks: whether to store the final dataset (input tracks + augmented ones)
+    :param recordings_made_by_us: whether the recording is made by us or not
     :return:
     """
     print("enrich_dataset>>>")
+    if recordings_made_by_us:
+        MAX_STDEV = 0.05
+        MIN_STDEV = 0.002
+    else:
+        MAX_STDEV = 0.025
+        MIN_STDEV = 0.001
     enriched_audio_tracks = {}
     for audio_fn in os.listdir(audio_dir):
         # Skip temporary files
@@ -94,28 +97,28 @@ def enrich_dataset(audio_dir, mode, n_noise, n_pitch, max_length=999999, store_t
                                                                n_noise)
                 # Add these tracks to the result dictionary
                 enriched_audio_tracks[audio_fn]['noise'] = noise_tracks
-                # Apply pitch shift only to the original audio
-                current_pitch_tracks = augment_audio_with_pitch_shift(audio_signal=original_signal,
-                                                                      sampling_rate=RATE,
-                                                                      min_pitch_shift=MIN_PITCH_STEP,
-                                                                      max_pitch_shift=MAX_PITCH_STEP,
-                                                                      n=n_pitch)
-                # Store them
-                enriched_audio_tracks[audio_fn]['pitch'] = current_pitch_tracks
+                if n_pitch > 0:
+                    # Apply pitch shift only to the original audio
+                    current_pitch_tracks = augment_audio_with_pitch_shift(audio_signal=original_signal,
+                                                                          sampling_rate=RATE,
+                                                                          min_pitch_shift=MIN_PITCH_STEP,
+                                                                          max_pitch_shift=MAX_PITCH_STEP,
+                                                                          n=n_pitch)
+                    # Store them
+                    enriched_audio_tracks[audio_fn]['pitch'] = current_pitch_tracks
                 if mode == "all_combinations":
                     # Create a list for storing the tracks obtained through pitch shift
                     pitch_noise_tracks = []
-                    # Iterate on the list with noise tracks
-                    for track in noise_tracks:
-                        current_pitch_tracks = augment_audio_with_pitch_shift(track,
-                                                                              RATE,
-                                                                              MIN_PITCH_STEP,
-                                                                              MAX_PITCH_STEP,
-                                                                              n_pitch)
-                        pitch_noise_tracks = pitch_noise_tracks + current_pitch_tracks
-                    # Add the tracks enriched with pitch shift to the current list
-                    enriched_audio_tracks[audio_fn]['pitch_noise'] = pitch_noise_tracks
-    if store_tracks:
-        np.savez(audio_dir + "augmented_tracks.npz", **enriched_audio_tracks)
+                    if n_pitch > 0:
+                        # Iterate on the list with noise tracks
+                        for track in noise_tracks:
+                            current_pitch_tracks = augment_audio_with_pitch_shift(track,
+                                                                                  RATE,
+                                                                                  MIN_PITCH_STEP,
+                                                                                  MAX_PITCH_STEP,
+                                                                                  n_pitch)
+                            pitch_noise_tracks = pitch_noise_tracks + current_pitch_tracks
+                        # Add the tracks enriched with pitch shift to the current list
+                        enriched_audio_tracks[audio_fn]['pitch_noise'] = pitch_noise_tracks
     print("enrich_dataset <<<")
     return enriched_audio_tracks
